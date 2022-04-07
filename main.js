cfa3949d614b1a6e98fe903fe17aaccbce992fe9
Ranor80 <bvasilj@gmail.com>
2022-04-07 10:22:32 +0200

    added function logWins

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 let choices;
 choices = ['paper','rock','scissors'];
+let winners = [];
 
 function game() {
 playerChoice()
+}
+function playRound() {
+    const playerSelection = playerChoice(); 
+    const compSelection = computerChoice();
+    const winner = checkWinner(playerSelection, compSelection);
+    winners.push(winner);
+
 }
 
 function playerChoice () {
@@ -20,7 +28,7 @@ while (check == false) {
     input = input.toLowerCase();
     check = validateInput (input);
 }
-console.log (input);
+return input;
 }
 
 
@@ -31,4 +39,29 @@ return choices (Math.floor(Math.random() * choices.length));
 function validateInput(choice) {
     return choices.includes(choice);
 }
-game();
+
+function checkWinner(choiceP, choiceC) {
+    if (choiceP ===choiceC) {
+        return ('Tie')
+    }   else if ((choiceP === 'rock' && choiceC === 'scissors') ||(choiceP === 'scissors' && choiceC === 'paper') || (choiceP === 'paper' && choiceC === 'rock'))    
+     { return ('Player');
+      } else return ('Computer');
+    }
+
+    function logWins() {
+        let playerWins = winners.filter((item) => item == "Player").length;
+        let computerWins = winners.filter((item) => item == "Computer").length;
+        let ties = winners.filter((item) => item == "Tie").length;
+        console.log("Results:");
+        console.log("Player Wins:", playerWins);
+        console.log("Computer Wins:", computerWins);
+        console.log("Ties:", ties);
+        
+function logRound(playerChoice, computerChoice, winner, round) {
+    console.log("Round:", round);
+    console.log("Player Chose:", playerChoice);
+    console.log("Computer Chose:", computerChoice);
+    console.log(winner, "Won the Round");
+    console.log("-------------------------------");
+  }
+
